3832f51f1ff495e26e38c2e3e4ac45c6c0e1e0a6
Vendetta <ananddechannel@gmail.com>
2024-11-13 15:01:40 +0900

    issue-1에 맞는 예시코드 작성

--- src/models/scheduleModel.js
+++ src/models/scheduleModel.js
@@ -20,20 +20,23 @@ const scheduleSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  startDate: {
-    // 시작일시
-    type: Date,
-    required: true,
-  },
-  endDate: {
-    // 종료일시
-    type: Date,
-    required: true,
+  date:
+  {
+    start: {
+      // 시작일시
+      type: Date,
+      required: true,
+    },
+    end: {
+      // 종료일시
+      type: Date,
+      required: true,
+    }
   },
   tag: {
     // 참석자
-    // 참석자의 태그값만 POST로 넘겨주는건가요?
     type: [String], // 배열 필드
+    required: true
   },
   calendarName: {
     // 캘린더
@@ -46,16 +49,10 @@ const scheduleSchema = new mongoose.Schema({
     // 일정추가 시 제목만 적어서 기록하는 경우도 있어서
     // required 속성 뺐습니다
   },
-  /* 
-        schAlarm
-        figma를 보니 ex)10분전 이렇게 입력을 하는 것 같은데
-        DB에 집어넣을때 endDate에서 해당시간 계산해서 넣어주면 될 것 같습니다
-    */
-  scdAlarm: {
-    // 알림
-    type: Date,
-    required: true,
-  },
+  schAlarm:  {
+    type: String,
+    required: true
+  }
 });
 
 export const Schedule = mongoose.model("Schedule", scheduleSchema);
