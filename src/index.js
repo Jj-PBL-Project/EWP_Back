5d0a3e0d95a4e51f8f898ce2e486476f8994c8c8
Vendetta <ananddechannel@gmail.com>
2024-11-13 14:43:37 +0900

    fix : 페이지 미사용
    REST API 만을 운영하기에
    보이는 화면은 Swagger만을 보여줍니다.

--- src/index.js
+++ src/index.js
@@ -19,10 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Page");
-});
-
 app.listen(80, () => {
   console.info("Server listening on port 80");
 });
