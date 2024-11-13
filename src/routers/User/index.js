bc8965f9b3b934823e9198eaf5ebc9eb95094cb3
Vendetta <ananddechannel@gmail.com>
2024-11-13 18:02:07 +0900

    feat : signup swagger 추가
    signup에 맞게 swagger 코드를 작성하였습니다.

--- src/routers/User/index.js
+++ src/routers/User/index.js
@@ -27,4 +27,56 @@ router.get("/test", (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * paths:
+ *  /api/user/signup:
+ *    post:
+ *      summary: "계정 생성"
+ *      description: "서버에 사용자 정보를 보내 계정을 생성합니다."
+ *      tags: [Users]
+ *      requestBody:
+ *       content:
+ *          application/json:
+ *              required: true
+ *              description: user token and memo info
+ *              schema:
+ *                type: object
+ *                properties:
+ *                   userName:
+ *                       type: string
+ *                       example: 홍길동
+ *                   userBirthday:
+ *                       type: string
+ *                       example: 2003-03-10
+ *                   userId:
+ *                      type: string
+ *                      example: testId
+ *                   userPassword:
+ *                       type: string
+ *                       example: admin1234
+ *      responses:
+ *        "201":
+ *          description: 유저 계정 생성 성공
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    message:
+ *                      type: String
+ *                      example: Sign Up Success
+ *        "400": 
+ *          description: 유저 계정 생성 실패
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    message:
+ *                      type: String
+ *                      example: Error Message
+ */
+router.post("/signup", signup);
+
 module.exports = router;
