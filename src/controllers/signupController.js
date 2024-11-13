204f854ef16a9bd3d788255c3a8d4c2a71eecc82
sch0426 <117507363+sch0426@users.noreply.github.com>
2024-11-13 17:41:06 +0900

    feat : 회원가입 기능 추가

---
+++ src/controllers/signupController.js
@@ -0,0 +1,25 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt"); // 비밀번호 암호화
+
+const signup = async (req, res) => {
+  const { userName, userBirthday, userId, userPassword } = req.body;
+  try {
+    // 비밀번호 암호화
+    const salt = await bcrypt.genSaltSync(10);
+    const hashPassword = await bcrypt.hashSync(req.body.userPassword, salt);
+
+    // 유저 데이터 생성 및 저장
+    const newUser = new User({
+      userName,
+      userBirthday,
+      userId,
+      userPassword: hashPassword,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "Sign Up Success" });
+  } catch (err) {
+    res.status(201).json({ message: err.message });
+  }
+};
+
+module.exports = { signup };
