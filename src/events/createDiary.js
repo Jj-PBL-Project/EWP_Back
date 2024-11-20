cd90fc33e73f11c024da472b455b294f4c5990d1
sch0426 <stubad1357@naver.com>
2024-11-20 17:12:59 +0900

    feat: 일기/일정 생성 기능

---
+++ src/events/createDiary.js
@@ -0,0 +1,31 @@
+const Diary = require("../models/diaryModel");
+const { v4 } = require("uuid");
+
+const createDiary = async (
+  socket,
+  { diaryTitle, diaryLocation, diaryDate, diaryContent }
+) => {
+  try {
+    // 일기 생성 및 저장
+    const newDiary = new Diary({
+      diaryTitle,
+      diaryLocation,
+      diaryDate,
+      diaryContent,
+      UUID: v4(),
+    });
+    await newDiary.save();
+    socket.emit("createDiaryRes", {
+      status: 201,
+      message: "일기 생성이 완료되었습니다.",
+    });
+  } catch (err) {
+    console.error(err);
+    socket.emit("createDiaryRes", {
+      status: 500,
+      message: "서버에서 오류가 발생했습니다.",
+    });
+  }
+};
+
+module.exports = createDiary;
