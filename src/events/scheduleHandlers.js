05ac2d9e42db3ef64e2c3898d22095728a9b750d
sch0426 <117507363+sch0426@users.noreply.github.com>
2024-11-21 16:40:17 +0900

    chore: 일기/일정 파일명 수정

---
+++ src/events/scheduleHandlers.js
@@ -0,0 +1,45 @@
+const Schedule = require("../models/scheduleModel");
+
+const createSchedule = async (
+  socket,
+  {
+    scdTitle,
+    isImportant,
+    scdLocation,
+    startDate,
+    endDate,
+    tag,
+    calendarName,
+    scdContent,
+    scdAlarm,
+  }
+) => {
+  try {
+    // 일정 생성 및 저장
+    const newSchedule = new Schedule({
+      scdTitle,
+      isImportant,
+      scdLocation,
+      startDate,
+      endDate,
+      tag,
+      calendarName,
+      scdContent,
+      scdAlarm,
+      UUID: socket.user.UUID,
+    });
+    await newSchedule.save();
+    socket.emit("createScheduleRes", {
+      status: 201,
+      message: "일정 생성이 완료되었습니다.",
+    });
+  } catch (err) {
+    console.error(err);
+    socket.emit("createScheduleRes", {
+      status: 500,
+      message: "서버에서 오류가 발생했습니다.",
+    });
+  }
+};
+
+module.exports = createSchedule;
