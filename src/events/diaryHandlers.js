0c80c73b19630bee0f194fd44af1df4f32026d57
sch0426 <117507363+sch0426@users.noreply.github.com>
2024-11-21 23:28:00 +0900

    feat: 일기 CRUD 기능 작성

--- src/events/diaryHandlers.js
+++ src/events/diaryHandlers.js
@@ -1,30 +1,132 @@
 const Diary = require("../models/diaryModel");
 
-const createDiary = async (
-  socket,
-  { diaryTitle, diaryLocation, diaryDate, diaryContent }
-) => {
+const diaryHandlers = async (socket, { type, data }) => {
   try {
-    // 일기 생성 및 저장
-    const newDiary = new Diary({
-      diaryTitle,
-      diaryLocation,
-      diaryDate,
-      diaryContent,
-      UUID: socket.user.UUID,
-    });
-    await newDiary.save();
-    socket.emit("createDiaryRes", {
-      status: 201,
-      message: "일기 생성이 완료되었습니다.",
-    });
+    console.log("서버로넘어왔다");
+    console.log(type, data);
+    switch (type) {
+      // 일기 생성 및 저장 CREATE
+      case "create":
+        const { diaryTitle, diaryLocation, diaryDate, diaryContent } = data;
+        console.log("diaryHandlers.js : ", {
+          diaryTitle,
+          diaryLocation,
+          diaryDate,
+          diaryContent,
+        });
+        const newDiary = new Diary({
+          diaryTitle,
+          diaryLocation,
+          diaryDate,
+          diaryContent,
+          UUID: socket.user.UUID,
+        });
+        await newDiary.save();
+        socket.emit("createDiaryRes", {
+          status: 201,
+          message: "일기 생성이 완료되었습니다.",
+        });
+        break;
+
+      // 일기 전체 조회 READ
+      case "readAll":
+        const diaries = await Diary.find();
+        socket.emit("readAllDiaryRes", {
+          status: 200,
+          message: "일기 조회를 완료했습니다.",
+          data: diaries,
+        });
+        break;
+
+      // 사용자가 선택한 일기 조회 READ
+      case "read":
+        const { readId } = data;
+        console.log("서버>클라로 넘겨준 readId", readId);
+        const diary = await Diary.findById(readId);
+
+        if (!diary) {
+          socket.emit("readDiaryRes", {
+            status: 404,
+            message: "해당 일기를 찾을 수 없습니다.",
+          });
+        } else {
+          socket.emit("readDiaryRes", {
+            status: 200,
+            message: "일기 조회를 완료했습니다.",
+            data: diary,
+          });
+        }
+        break;
+
+      // 일기 수정 UPDATE
+      case "update":
+        const { updateId, updateDiaryTitle, updateDiaryContent } = data;
+        // 수정 사항만 추가
+        const updateFields = {};
+        if (updateDiaryTitle !== undefined)
+          updateFields.diaryTitle = updateDiaryTitle;
+        if (updateDiaryContent !== undefined)
+          updateFields.diaryContent = updateDiaryContent;
+
+        if (Object.keys(updateFields).length === 0) {
+          socket.emit("updateDiaryRes", {
+            status: "error",
+            message: "수정사항이 없습니다.",
+          });
+          break;
+        }
+
+        const updateDiary = await Diary.findByIdAndUpdate(
+          updateId,
+          updateFields,
+          { new: true }
+        );
+        if (!updateDiary) {
+          socket.emit("updateDiaryRes", {
+            status: 404,
+            message: "일기를 찾을 수 없습니다.",
+          });
+        } else {
+          socket.emit("updateDiaryRes", {
+            status: 200,
+            message: "일기 수정이 완료되었습니다.",
+            data: updateDiary,
+          });
+        }
+        break;
+
+      // 일기 삭제 DELETE
+      case "delete":
+        const { deleteId } = data;
+        const deleteDiary = await Diary.findOneAndDelete(deleteId);
+
+        if (!deleteDiary) {
+          socket.emit("deleteDiaryRes", {
+            status: 404,
+            message: "일기를 찾을 수 없습니다.",
+          });
+        } else {
+          socket.emit("deleteDiaryRes", {
+            status: 200,
+            message: "일기 삭제가 완료되었습니다.",
+          });
+        }
+        break;
+
+      default:
+        socket.emit("diaryRes", {
+          status: 400,
+          message: "유효하지 않은 타입입니다.",
+        });
+        break;
+    }
   } catch (err) {
     console.error(err);
-    socket.emit("createDiaryRes", {
+    socket.emit("diaryRes", {
       status: 500,
       message: "서버에서 오류가 발생했습니다.",
     });
   }
 };
 
-module.exports = createDiary;
+module.exports = diaryHandlers;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
-const express = require('express');
-const { createServer } = require('http');
+const express = require("express");
+const { createServer } = require("http");
 const app = express();
 const httpServer = createServer(app);
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const { readdirSync } = require("fs");
 const io = new Server(httpServer, {
-    pingInterval: 5000,
-    pingTimeout: 120000
+  pingInterval: 5000,
+  pingTimeout: 120000,
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 global.io = io;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/test/index.html');
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/test/index.html");
+});
 
 io.on("connection", async (socket) => {
-    console.log(socket.id + "is connected");
+  console.log(socket.id + "is connected");
 
-    const events = readdirSync("./events");
-    events.forEach((file) => {
-        const event = require("./events/" + file);
-        socket.on(file.replace(".js", ""), (data) => event(socket, data));
+  const events = readdirSync("./events");
+  events.forEach((file) => {
+    const event = require("./events/" + file);
+    socket.on(file.replace(".js", ""), (data) => {
+      event(socket, data);
     });
+  });
 });
 
 mongoose
-    .connect("mongodb://localhost:27017/ewp_test")
-    .then(() => {
-        console.log("ewp_test DB에 연결 성공!");
-    })
-    .catch((err) => {
-        throw new Error("데이터베이스 연결에 실패했습니다.");
-    });
+  .connect("mongodb://localhost:27017/ewp_test")
+  .then(() => {
+    console.log("ewp_test DB에 연결 성공!");
+  })
+  .catch((err) => {
+    throw new Error("데이터베이스 연결에 실패했습니다.");
+  });
 
 httpServer.listen(80, () => {
-    console.log("Server listening on port 80");
+  console.log("Server listening on port 80");
 });
